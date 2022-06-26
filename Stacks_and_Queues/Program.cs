577c571b721e6596ba71a4643728d10276a610be
Prateek Pai <paiprateek123@gmail.com>
2022-06-26 12:31:25 +0530

    Create stack of 56 -> 30 -> 70

--- Stacks_and_Queues/Program.cs
+++ Stacks_and_Queues/Program.cs
@@ -2,11 +2,45 @@
 
 namespace Stacks_and_Queues
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stacks and Queues Problem ");
+			Stack stack = new Stack(3);
+			stack.Push(56);
+			stack.Push(30);
+			stack.Push(70);
+
+			
+
         }
-    }
+		// Craeting Stack class
+		public class Stack
+		{
+			int capacity;
+			int[] obj;
+			int top = -1;
+			public Stack(int capacity)
+			{
+				this.capacity = capacity;
+				obj = new int[capacity];
+			}
+
+
+			//Creating method to push data into stack
+			public bool Push(int data)
+			{
+				if (top == capacity - 1)
+				{
+					Console.WriteLine("Stack overFlow");
+					return false;
+				}
+				obj[++top] = data;
+				Console.WriteLine(data);
+				return true;
+			}
+
+
+		}
+	}
 }

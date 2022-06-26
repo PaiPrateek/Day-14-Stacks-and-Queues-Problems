8b0db8ebda9b0f389b93ac1b13569adffb4c1321
Prateek Pai <paiprateek123@gmail.com>
2022-06-26 12:47:22 +0530

    Peek and Pop from the stack until it is Empty

--- Stacks_and_Queues/Program.cs
+++ Stacks_and_Queues/Program.cs
@@ -7,19 +7,53 @@ namespace Stacks_and_Queues
         static void Main(string[] args)
         {
 			Stack stack = new Stack(3);
+
+            Console.WriteLine("Creating Stack: ");
+			// Adding Element into the stack
 			stack.Push(56);
 			stack.Push(30);
 			stack.Push(70);
 
-			
+            Console.WriteLine("\n");
 
-        }
+
+			// Dispaly the latest added element
+			var TC1=stack.Peek();
+            Console.WriteLine("Peek :" + TC1);
+			Console.WriteLine("\n");
+
+			// Remove the latest added element
+			var TC2=stack.Pop();
+			Console.WriteLine("Pop :" + TC2);
+
+			// Remove the element untill stack is empty
+			var TC3 = stack.Peek();
+			Console.WriteLine("Peek :" + TC3);
+			Console.WriteLine("\n");
+
+			var TC4 = stack.Pop();
+			Console.WriteLine("Pop :" + TC4);
+
+			var TC5 = stack.Peek();
+			Console.WriteLine("Peek :" + TC5);
+			Console.WriteLine("\n");
+
+			var TC6 = stack.Pop();
+			Console.WriteLine("Pop :" + TC6);
+
+			var TC7 = stack.Peek();
+			Console.WriteLine("Peek :" + TC7);
+
+
+		}
 		// Craeting Stack class
 		public class Stack
 		{
 			int capacity;
 			int[] obj;
 			int top = -1;
+
+			// Creating Stack method 
 			public Stack(int capacity)
 			{
 				this.capacity = capacity;
@@ -40,6 +74,27 @@ namespace Stacks_and_Queues
 				return true;
 			}
 
+			// Creating Pop() method to remove the latest added element from the stack
+			public int Pop()
+			{
+				if (top == -1)
+				{
+					Console.WriteLine("Stack Underflow");
+					return default(int);
+				}
+				return obj[top--];
+			}
+
+			// Creating Peek() method to return th leatest added elemet in the stack
+			public int Peek()
+			{
+				if (top == -1)
+				{
+					Console.WriteLine("Stack is UnderFlow");
+					return (int)default;
+				}
+				return obj[top];
+			}
 
 		}
 	}
